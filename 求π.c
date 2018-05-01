607c0ce562782ec7021de7cd56864710434ee3a2
X-Perseverance <1742763760@qq.com>
2018-05-01 11:44:09 +0800

    格雷戈里公式

---
+++ 求π.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//¦Ð = 4*(1-1/3+1/5-1/7 ...)
+
+double Sum()
+{
+	int a = 1, b = 1;
+	double num = 1.0, sum = 0;
+	while (fabs(num) >= 0.0000001)
+	{
+		num = a*1.0 / b;
+		sum += num;
+		a = -a;
+		b += 2;
+	}
+	return sum;
+}
+
+int main()
+{
+	printf("¦ÐÔ¼Îª£º%f\n", 4 * Sum());
+	system("pause");
+	return 0;
+}
