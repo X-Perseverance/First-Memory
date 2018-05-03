1b66dc67555f3b8e9f9529cbda0b5362c14f96fb
X-Perseverance <1742763760@qq.com>
2018-05-03 11:40:18 +0800

    递归方式

---
+++ 依次输出整数的每一位.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void Print(int num)
+{
+	if (num > 9)
+	{
+		Print(num / 10); //ตÝน้
+	}
+	printf("%d ", num % 10);
+}
+
+int main()
+{
+	int num = 123;
+	Print(num);
+	printf("\n");
+	system("pause");
+	return 0;
+}
