0095c31dc7c4c98a2cbe12b5b5748011cf549c6f
X-Perseverance <1742763760@qq.com>
2018-05-30 22:43:16 +0800

    花括号匹配问题

---
+++ 花括号匹配.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int ch = 0;
+	int count = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		if (ch == '{')
+		{
+			count++;
+		}
+		if (ch == '}' && count == 0)
+		{
+			printf("≤ª∆•≈‰\n");
+			system("pause");
+			return 0;
+		}
+		if (ch == '}')
+		{
+			count--;
+		}
+	}
+	if (count == 0)
+	{
+		printf("∆•≈‰\n");
+	}
+  system("pause");
+  return 0;
+}
