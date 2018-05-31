a4308fa9aeb9c640a410e442cfc906feb63a27b7
X-Perseverance <37230450+X-Perseverance@users.noreply.github.com>
2018-05-31 17:58:19 +0800

    三种方式

--- 大小写字符转换.c
+++ 大小写字符转换.c
@@ -3,32 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//编写一个程序，可以一直接收键盘字符
-//如果是小写字符就输出对应的大写字符；如果是大写字符就输出对应的小写字符；如果是数字不输出
-
 int main()
 {
 	char ch = 0;
-	printf("请输入字符：");
+	printf("璇疯緭鍏ュ瓧绗︼細");
 	while ((ch = getchar()) != '\n')
 	{
-		if ((ch >= '0') && (ch <= '9'))
+		if ((ch >= 'A') && (ch <= 'Z'))
 		{
-			continue;
+			//ch = ch - 'A' + 'a';
+			//ch = ch + 32;
+			putchar(tolower(ch));
 		}
-		else
+		else if ((ch >= 'a') && (ch <= 'z'))
 		{
-			if ((ch >= 'A') && (ch <= 'Z'))
-			{
-				ch = ch - 'A' + 'a';
-			}
-			else
-			{
-				ch = ch - 'a' + 'A';
-			}
-			putchar(ch);
+			//ch = ch - 'a' + 'A';
+			//ch = ch - 32;
+			putchar(toupper(ch));
 		}
+		//putchar(ch);
 	}
+	printf("\n");
 	system("pause");
 	return 0;
 }
